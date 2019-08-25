4c2a3c98049452ebc6b25e2ae70f2b39528fd0d2
Volchar <volchar@gmail.com>
2019-08-25 23:37:45 +0300

    Make new program - first itteration

--- functionsTable.js
+++ functionsTable.js
@@ -17,34 +17,34 @@ function startTabl() {
 
 switch(imgLevel) {
   case "dig0.png":
-    return "0";
+    return 0;
     break;
   case "dig1.png":
-    return "1";
+    return 1;
     break;
   case "dig2.png":
-    return "2";
+    return 2;
     break;
   case "dig3.png":
-    return "3";
+    return 3;
     break;
   case "dig4.png":
-    return "4";
+    return 4;
     break;  
   case "dig5.png":
-    return "5";
+    return 5;
     break;
   case "dig6.png":
-    return "6";
+    return 6;
     break;
   case "dig7.png":
-    return "7";
+    return 7;
     break;
   case "dig8.png":
-    return "8";
+    return 8;
     break;
   case "dig9.png":
-    return "9";
+    return 9;
     break;
 }
 
