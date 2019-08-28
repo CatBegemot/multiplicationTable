c424749a203a8daa5d872fa4f495eee5935ed82b
Volchar <volchar@gmail.com>
2019-08-29 01:13:51 +0300

    Add texts and try include function to choise digital

--- functionsTable.js
+++ functionsTable.js
@@ -1,38 +1,44 @@
-function startTabl() {
-    var viz = document.getElementById("imgLevel");
-    viz.style.visibility = "visible";
+function getNum() {
+  
+var imgLevel = document.createElement("img");
+  divLevel.appendChild(imgLevel);
 switch(imgLevel) {
-  case "dig0.png":
-    return 0;
-    break;
-  case "dig1.png":
-    return 1;
-    break;
-  case "dig2.png":
-    return 2;
-    break;
-  case "dig3.png":
-    return 3;
-    break;
-  case "dig4.png":
-    return 4;
-    break;  
-  case "dig5.png":
-    return 5;
-    break;
-  case "dig6.png":
-    return 6;
-    break;
-  case "dig7.png":
-    return 7;
-    break;
-  case "dig8.png":
-    return 8;
-    break;
-  case "dig9.png":
-    return 9;
-    break;
+ case "dig0.png":
+  return 0;
+  break;
+ case "dig1.png":
+  return 1;
+  break;
+ case "dig2.png":
+  return 2;
+  break;
+ case "dig3.png":
+  return 3;
+  break;
+ case "dig4.png":
+  return 4;
+  break;
+ case "dig5.png":
+  return 5;
+  break;
+ case "dig6.png":
+  return 6;
+  break;
+ case "dig7.png":
+  return 7;
+  break;
+ case "dig8.png":
+  return 8;
+  break;
+ case "dig9.png":
+  return 9;
+  break;
 }
 
+}
 
-tens = getNum(tensImg)
+//var imgLevel = document.createElement("img");
+//  document.body.appendChild(imgLevel);
+//  divLevel.id = "imgLevel";
+//  divLevel.appendChild(imgLevel);
+//tens = getNum(imgLevel);
--- tableMulty.js
+++ tableMulty.js
@@ -1,25 +1,37 @@
 ﻿var imgBackgroud = document.createElement("img");
-imgBackgroud.id = "imgBackgroud";
+  imgBackgroud.id = "imgBackgroud";
 document.body.appendChild(imgBackgroud);
-  imgBackgroud.style.zIndex = 0;
+  imgBackgroud.style.zIndex = -1;
   imgBackgroud.src = imgPath + "kletka.png";
 
 var divLevel = document.createElement("div");
 divLevel.id = "divLevel";
 document.body.appendChild(divLevel);
+var textBegin = document.createElement("p");
+  textBegin.id = "textBegin";
+  divLevel.appendChild(textBegin);
+  textBegin.style.fontFamily = "Mistral";
+  textBegin.zIndex = 1;
+  textBegin.style.position = "absolute";
+  textBegin.style.top = "-45px";
+  textBegin.style.color = "darkblue";
+  textBegin.style.fontsize = "60px";
+  textBegin.innerHTML = "It`s a test for multiplication table";
+var imgLevel = document.createElement("img");
+imgLevel.id = "imgLevel";
+  divLevel.appendChild(imgLevel);
   divLevel.style.position = "absolute";
   divLevel.style.zIndex = 1;
   divLevel.style.top = "95px";
   divLevel.style.left = "95px";
-  
+ 
 var index;
   for (var index = 0; index <= 9; index++) {
-  var imgLevel = document.createElement("img");
+var imgLevel = document.createElement("img");
   divLevel.appendChild(imgLevel);
-  imgLevel.style.border = "1px solid #3eff03";
   imgLevel.style.margin = "5px";
   imgLevel.style.left = (45*index) +"px";
-  imgLevel.src = imgPath +  "dig" + index + ".png";
+  imgLevel.src = imgPath + "dig" + index + ".png";
 }
 
 var divMain = document.createElement("div");
@@ -29,7 +41,20 @@ document.body.appendChild(divMain);
   divMain.style.zIndex = 1;
   divMain.style.top = "200px";
   divMain.style.left = "100px";
-  divMain.style.border = "1px solid #3eff03";
+var textChoise = document.createElement("p");
+  textChoise.id = "textChoise";
+  divMain.appendChild(textChoise);
+  textChoise.style.fontFamily = "Mistral";
+  textChoise.style.color = "darkblue";
+  textChoise.innerHTML = "Choose the value of the first factor";
+
+
+var imgMainMulty = document.createElement("img");
+  divMain.appendChild(imgMainMulty);
+
+imgLevel.onclick = function() {getNum(imgMainMulty)};
+  imgMainMulty.src = imgPath + "case"; 
+  
 
 var divSelector = document.createElement("div");
 divSelector.id = "divSelector";
@@ -48,5 +73,3 @@ document.body.appendChild(divScore);
   divScore.style.top = "400px";
   divScore.style.left = "100px";
   divScore.style.border = "1px solid #3eff03";
-
-// imgMulty.onclick = function() {getNum(imgMultySrc)};
